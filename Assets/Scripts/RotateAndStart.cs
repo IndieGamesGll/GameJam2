2251fcfd29f6358401b2b3e69b52f04762420870
nik <niknovikov076@gmail.com>
2022-11-10 19:54:13 +0300

    замена
    добавил работу с камерой, изменил текстуру перса

--- Assets/Scripts/RotateAndStart.cs
+++ Assets/Scripts/RotateAndStart.cs
@@ -6,10 +6,13 @@ public class RotateAndStart : MonoBehaviour
 {
 
     [SerializeField] Transform _player;
+    [SerializeField] Transform _camera;
     [SerializeField] Animator _animator;
     [SerializeField] PlayerController _playerController;
     public void RotateAndGo()
     {
+        _camera.position = new Vector3(0, 6.19f, -10);
+        _camera.rotation = Quaternion.Euler(16.194f,0f,0f);
         _player.Rotate(0, 180, 0);
         _animator.SetBool("Dance", false);
         _playerController._speed = 20f;
